e973ebb61fd766dbaf73ecec76c190a3babb7512
vrnandr <vrnandr@mail.ru>
2018-02-12 16:43:47 +0400

    asynctask2

--- app/src/main/java/com/example/andrey/myapplication/ZNO.java
+++ app/src/main/java/com/example/andrey/myapplication/ZNO.java
@@ -1,6 +1,9 @@
 package com.example.andrey.myapplication;
 
+import java.util.Date;
+
 class ZNO {
+    transient Date datestamp;
     String SDCIINFO;
     String SDESPPID;
     String SDCLASSIF;
@@ -11,4 +14,46 @@ class ZNO {
     String SDINFO;
     String SDCREATED;
     String SDDEADLINE;
+
+    @Override
+    public int hashCode(){
+        return SDTASKID.hashCode();
+    }
+
+    @Override
+    public boolean equals (Object obj){
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        ZNO other = (ZNO) obj;
+        if (!SDTASKID.equals(other.SDTASKID))
+            return false;
+        if (!SDCIADDR.equals(other.SDCIADDR))
+            return false;
+        if (!SDCIINFO.equals(other.SDCIINFO))
+            return false;
+        if (!SDCLASSIF.equals(other.SDCLASSIF))
+            return false;
+        if (!SDCREATED.equals(other.SDCREATED))
+            return false;
+        if (!SDDEADLINE.equals(other.SDDEADLINE))
+            return false;
+        if (!SDESPPID.equals(other.SDESPPID))
+            return false;
+        if (!SDINFO.equals(other.SDINFO))
+            return false;
+        if (!SDSERVICE.equals(other.SDSERVICE))
+            return false;
+        if (!SDSTATUS.equals(other.SDSTATUS))
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString(){
+        return SDTASKID;
+    }
 }
